9d9668d8b37ae4a151f5ac6d2f8f364f8032e258
MoumitaDutta13 <157048848+MoumitaDutta13@users.noreply.github.com>
2024-02-03 03:48:26 +0530

    Add files via upload

---
+++ quiz-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Quiz from './assets/Components/QuizComponents/Quiz'
+
+const App = () => {
+  return (
+    <div>
+      <Quiz/>
+    </div>
+  )
+}
+
+export default App
+
